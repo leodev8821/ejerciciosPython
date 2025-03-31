1756975195568e5390a749b994feb120fc098520
leodev8821 <jaime.leo.caicedo@gmail.com>
2025-03-31 17:53:07 +0200

    Normalizacion finalizada

--- ejercicio_dataset/normalize_functions.py
+++ ejercicio_dataset/normalize_functions.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from rapidfuzz import process, fuzz
+from rapidfuzz import process, fuzz, utils
 import json
 import os
 
@@ -20,7 +20,7 @@ def normalize_age_range(df):
         '35-44': 4,
         '45-54': 5,
         '55-64': 6,
-        '65 or over': 6
+        '65 or over': 7
     }
     # Renombro la columna con el de 'age_range_id'
     df = df.rename(columns={column_name: 'age_range_id'})
@@ -31,18 +31,113 @@ def normalize_age_range(df):
 
     return df
 
-def normalize_industries(df):
+# Normaliza el rango de experiencia en años
+# Return --> Un DF con una columna 'overall_experience_years_id' y su id equivalente
+def normalized_work_experience(df):
+    # Si es serie, lo convierto a DF
     if isinstance(df, pd.Series):
-        df = df.to_frame(name='normalized_industries')
+        df = df.to_frame()
+    # Extraigo el nombre de la columna del DF
+    column_name = df.columns[0]
+    
+    # mapeo de los rangos de edad con su id
+    experience_mapping = {
+        "1 year or less" : 1,
+        "2 - 4 years" :	2,
+        "5-7 years" : 3,
+        "8 - 10 years" : 4,
+        "11 - 20 years" : 5,
+        "21 - 30 years" : 6,
+        "31 - 40 years" : 7,
+        "41 years or more" : 8
+    }
+    # Renombro la columna con el de 'age_range_id'
+    df = df.rename(columns={column_name: 'experience_years_id'})
+
+    # Voy creando a partir de reemplazar valores en el nuevo DF
+    with pd.option_context('future.no_silent_downcasting', True):
+        df['experience_years_id'] = df['experience_years_id'].replace(experience_mapping)
+
+    return df
+
+# Normaliza el rango de estudios
+# Return --> Un DF con una columna 'highest_education_level_id' y su id equivalente
+def normalized_highest_education_level(df):
+    # Si es serie, lo convierto a DF
+    if isinstance(df, pd.Series):
+        df = df.to_frame()
+    # Extraigo el nombre de la columna del DF
+    column_name = df.columns[0]
+    
+    # mapeo de los rangos de edad con su id
+    education_mapping = {
+        "no_data" : 0,
+        "High School" : 1,
+        "Some college" : 2,
+        "College degree" : 3,
+        "Master's degree" : 4,
+        "Professional degree (MD, JD, etc.)" : 5,
+        "PhD" : 6
+    }
+    # Renombro la columna con el de 'age_range_id'
+    df = df.rename(columns={column_name: 'highest_education_level_id'})
+
+    # Voy creando a partir de reemplazar valores en el nuevo DF
+    with pd.option_context('future.no_silent_downcasting', True):
+        df['highest_education_level_id'] = df['highest_education_level_id'].replace(education_mapping)
+
+    return df
+
+# Normaliza el rango de genero
+# Return --> Un DF con una columna 'gender_id' y su id equivalente
+def normalized_gender(df):
+    # Si es serie, lo convierto a DF
+    if isinstance(df, pd.Series):
+        df = df.to_frame()
+    # Extraigo el nombre de la columna del DF
+    column_name = df.columns[0]
+    
+    # mapeo de los rangos de edad con su id
+    gender_mapping = {
+        "no_data" : 0,
+        "Man": 1,
+        "Woman": 2,
+        "Non-binary": 3,
+        "Other or prefer not to answer": 4,
+        "Prefer not to answer": 4
+    }
+    # Renombro la columna con el de 'age_range_id'
+    df = df.rename(columns={column_name: 'gender_id'})
+
+    # Voy creando a partir de reemplazar valores en el nuevo DF
+    with pd.option_context('future.no_silent_downcasting', True):
+        df['gender_id'] = df['gender_id'].replace(gender_mapping)
+
+    return df
+
+# Normalizar los textos con los indicados en un json
+def normalize_text(df, df_name, option):
+    if isinstance(df, pd.Series):
+        df = df.to_frame(name=df_name)
     elif isinstance(df, pd.DataFrame):
-        df = df.iloc[:, 0].to_frame('normalized_industries')
+        df = df.iloc[:, 0].to_frame(df_name)
     else:
         raise ValueError("El parámetro debe ser un Series o DataFrame de pandas")
     
-    df['normalized_industries'] = df['normalized_industries'].apply(normalize_text)
+    match option:
+        case 1:
+            df[df_name] = df[df_name].apply(convert_industries_text)
+        case 2:
+            df[df_name] = df[df_name].apply(convert_currency_text)
+            df = normalize_currency(df)
+        case 3:
+            df[df_name] = df[df_name].apply(convert_work_country_text)
+        case 4:
+            df[df_name] = df[df_name].apply(convert_us_states_text)
+
     return df
 
-def normalize_text(text):
+def convert_industries_text(text):
     script_dir = os.path.dirname(__file__)
     json_path = os.path.join(script_dir, 'json_patterns', 'industries.json')
         
@@ -71,6 +166,96 @@ def normalize_text(text):
             
     return 'other'
 
+
+def convert_currency_text(text):
+    script_dir = os.path.dirname(__file__)
+    json_path = os.path.join(script_dir, 'json_patterns', 'other_currencies.json')
+        
+    with open(json_path, 'r', encoding='utf-8') as archive:
+        replacement_groups = json.load(archive)
+
+    if pd.isna(text) or not isinstance(text, str):
+        return 'n/a'
+    
+    text = text.strip().lower()
+    if not text:
+        return 'n/a'
+    
+    for replacement, targets in replacement_groups.items():
+        result = process.extractOne(
+            text,
+            targets,
+            scorer=fuzz.WRatio,
+            processor=utils.default_process
+        )
+        if result is None:
+            continue
+            
+        best_match, score, _ = result
+        if score >= 90:
+            return replacement
+            
+    return 'other'
+
+def convert_work_country_text(text):
+    script_dir = os.path.dirname(__file__)
+    json_path = os.path.join(script_dir, 'json_patterns', 'work_country.json')
+        
+    with open(json_path, 'r', encoding='utf-8') as archive:
+        replacement_groups = json.load(archive)
+
+    if pd.isna(text) or not isinstance(text, str):
+        return 'unknown'
+    
+    text = text.strip().lower()
+    if not text:
+        return 'unknown'
+    
+    for replacement, targets in replacement_groups.items():
+        result = process.extractOne(
+            text,
+            targets,
+            processor=None
+        )
+        if result is None:
+            continue
+            
+        best_match, score, _ = result
+        if score >= 80:
+            return replacement
+            
+    return 'other'
+
+def convert_us_states_text(text):
+    script_dir = os.path.dirname(__file__)
+    json_path = os.path.join(script_dir, 'json_patterns', 'us_states.json')
+        
+    with open(json_path, 'r', encoding='utf-8') as archive:
+        replacement_groups = json.load(archive)
+
+    if pd.isna(text) or not isinstance(text, str):
+        return 'unknown'
+    
+    text = text.strip().lower()
+    if not text:
+        return 'n/a'
+    
+    for replacement, targets in replacement_groups.items():
+        result = process.extractOne(
+            text,
+            targets,
+            processor=None
+        )
+        if result is None:
+            continue
+            
+        best_match, score, _ = result
+        if score >= 80:
+            return replacement
+            
+    return 'n/a'
+
+
 # Normaliza el currency
 # Return --> Un DF con una columna 'currency_id' y su id equivalente
 def normalize_currency(df):
@@ -82,7 +267,7 @@ def normalize_currency(df):
     
     # mapeo de los currency con su id
     script_dir = os.path.dirname(__file__)
-    json_path = os.path.join(script_dir, 'json_patterns', 'industries.json')
+    json_path = os.path.join(script_dir, 'json_patterns', 'currencies.json')
         
     with open(json_path, 'r', encoding='utf-8') as archive:
         replacement_groups = json.load(archive)
@@ -93,5 +278,4 @@ def normalize_currency(df):
     # Voy creando a partir de reemplazar valores en el nuevo DF
     with pd.option_context('future.no_silent_downcasting', True):
         df['currency_id'] = df['currency_id'].replace(replacement_groups)
-
     return df
--- ejercicio_dataset/propia_pagina.py
+++ ejercicio_dataset/propia_pagina.py
@@ -10,97 +10,163 @@ def part1():
     time_stamp_df = rdf.iloc[:, 0]
 
     # Columna 1 age_range
-    age_range_df = rdf.iloc[:, 1]
+    age_range_df = rdf.iloc[:, 1].fillna('no_data')
 
     # Columna 2 con el rango normalizado age_range_id
     age_range_id_df = normalize_age_range(age_range_df)
 
     # Columna 3 indrustries
-    industries_df = rdf.iloc[:,2].fillna('no_value')
+    industries_df = rdf.iloc[:,2].fillna('no_data')
 
     # Columna 4 normalized_industries
-    normalized_industries_df = normalize_industries(industries_df)
+    normalized_industries_df = normalize_text(industries_df,'normalized_industries',1)
 
     # Columna 5 job_title
-    job_title_df = rdf.iloc[:,3].fillna('no_title')
+    job_title_df = rdf.iloc[:,3].fillna('no_data')
 
     # Columna 6 job_title_context
-    job_title_context_df = rdf.iloc[:,4].fillna('no_context')
+    job_title_context_df = rdf.iloc[:,4].fillna('no_data')
 
     # Columna 7 anual_salary
-    anual_salary_df = rdf.iloc[:,5]
+    anual_salary_df = rdf.iloc[:,5].fillna(0)
 
     # Columna 8 aditional_monetary
-    aditional_monetary_df = rdf.iloc[:,6]
+    aditional_monetary_df = rdf.iloc[:,6].fillna(0)
 
     # Columna 9 currency
-    currency_df = rdf.iloc[:,7]
+    currency_df = rdf.iloc[:,7].fillna('no_data')
 
     # Columna 10 con el rango normalizado currency_id
     currency_id_df = normalize_currency(currency_df)
 
+    # Columna 11 con other_currencies
+    other_currency_df = rdf.iloc[:,8].fillna('no_data')
+
+    # Columna 12 con el rango normalizado other_currency_id
+    other_currency_id_df = normalize_text(other_currency_df,'other_currencies_id',2)
+
+    # Columna 13 income_context
+    income_context_df = rdf.iloc[:,9].fillna('no_data')
+
+    # Columna 13 work_country
+    work_country_df = rdf.iloc[:,10].fillna('no_data')
+
+    # Columna 14 normalized_work_country
+    normalized_work_country_df = normalize_text(work_country_df,'normalized_work_country',3)
+
+    # Columna 15 work_country
+    us_states_df = rdf.iloc[:,11].fillna('no_data')
+
+    # Columna 16 normalized_work_country
+    normalized_us_states_df = normalize_text(us_states_df,'normalized_us_states',4)
+
+    # Columna 17 us_work_city
+    us_work_city_df = rdf.iloc[:,12].fillna('no_data')
+
+    # Columna 18 overall_work_experience
+    overall_work_experience_df = rdf.iloc[:,13].fillna('no_data')
+
+    # Columna 19 con el rango normalizado overall_work_experience_id
+    overall_work_experience_id_df = normalized_work_experience(overall_work_experience_df)
+
+    # Columna 20 overall_work_experience
+    field_work_experience_df = rdf.iloc[:,14].fillna('no_data')
+
+    # Columna 21 con el rango normalizado field_work_experience_id
+    field_work_experience_id_df = normalized_work_experience(field_work_experience_df)
+
+    # Columna 22 highest_education_level
+    highest_education_level_df = rdf.iloc[:,15].fillna('no_data')
+
+    # Columna 23 con el rango normalizado highest_education_level_id
+    highest_education_level_id_df = normalized_highest_education_level(highest_education_level_df)
+
+    # Columna 24 gender
+    gender_df = rdf.iloc[:,16].fillna('no_data')
+
+    # Columna 23 con el rango normalizado gender_id
+    gender_id_df = normalized_gender(gender_df)
+
+    # Columna 25 race
+    race_df = rdf.iloc[:,17].fillna('no_data')
+
     #unir todos los dataframe
     final_df = pd.concat([
         time_stamp_df,
-        age_range_df,
         age_range_id_df,
-        industries_df,
+        age_range_df,
         normalized_industries_df,
+        industries_df,
         job_title_df,
         job_title_context_df,
         anual_salary_df,
         aditional_monetary_df,
+        currency_id_df,
         currency_df,
-        currency_id_df
+        other_currency_id_df,
+        other_currency_df,
+        income_context_df,
+        normalized_work_country_df,
+        work_country_df,
+        normalized_us_states_df,
+        us_states_df,
+        us_work_city_df,
+        overall_work_experience_id_df,
+        overall_work_experience_df,
+        field_work_experience_id_df,
+        field_work_experience_df,
+        highest_education_level_id_df,
+        highest_education_level_df,
+        gender_id_df,
+        gender_df,
+        race_df
     ], axis=1)
 
     final_df.columns = [
         'time_stamp', 
-        'age_range', 
         'age_range_id', 
-        'work_industries', 
+        'age_range', 
         'normalized_industries', 
+        'work_industries', 
         'job_title', 
         'job_title_context',
         'anual_salary',
         'aditional_monetary',
+        'currency_id',
         'currency',
-        'currency_id'
+        'other_currency_id',
+        'other_currency',
+        'income_context',
+        'normalized_work_country',
+        'work_country',
+        'normalized_us_states',
+        'us_states',
+        'us_work_city',
+        'overall_work_experience_id',
+        'overall_work_experience',
+        'field_work_experience_id',
+        'field_work_experience',
+        'highest_education_level_id',
+        'highest_education_level',
+        'gender_id',
+        'gender',
+        'race'
         ]
 
     # almacenar los dataframe en un txt
-    with open("clean_df.txt", "w", encoding='utf-8') as file:
-        file.write(final_df.to_string(index=False))
+    # with open("clean_df.txt", "w", encoding='utf-8') as file:
+    #     file.write(final_df.to_string(index=False))
         
-    file.close()
+    # file.close()
 
-    # cleaned_df.columns = [
-    #     'aditional_monetary',
-    #     'currency',
-    #     'other_currency',
-    #     'income_context',
-    #     'country',
-    #     'usa_state',
-    #     'work_city',
-    #     'total_experience_years',
-    #     'field_experience_years',
-    #     'educational_level',
-    #     'gender',
-    #     'race'
-    #     ]
-     
-
-    
-
-    # patronAcademic = r'^academic[-/\s]+(.*)'
-    # patronAdmin = r'administration[-/\s]+(.*)'
-    # patronAerospace = r'aerospace[-/\s]+(.*)'
-    # patronArchaeology = r'archaeology[-/\s]+(.*)'
-
-    # my_df = normalize(my_df=my_df, mask=mask_academic, regex=patronAcademic)
-    # my_df = normalize(my_df=my_df, mask=mask_admin, regex=patronAdmin)
-    # my_df = normalize(my_df=my_df, mask=mask_aerospace, regex=patronAerospace)
-    # my_df = normalize(my_df=my_df, mask=mask_archaeology, regex=patronArchaeology)
+    # Si el Excel no existe, lo crea
+    if not os.path.exists('normalized_answer.xlsx'):
+        with pd.ExcelWriter('normalized_answer.xlsx',mode='w',engine='openpyxl') as writer:
+            final_df.to_excel(
+                writer,
+                sheet_name='normalized_answer',
+                index=False
+            )
 
     # # Obtener industria base y agrupar
     # my_df['Industries'] = my_df['Industries'].str.extract(r'^\s*([^-/\s]+)', expand=False)
@@ -111,14 +177,6 @@ def part1():
     #     .reset_index()
     #     .rename(columns={'specialization_temp': 'specialization'})
     # )
-
-    # with open("clean_df.txt", "w", encoding='utf-8') as file:
-    #     file.write(df_resultado.to_string(index=False))
-        
-    # file.close()
-
-    #print(df_resultado)
-
 
 part1()
  
